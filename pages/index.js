d314869572fb2933c736798a7ccb750d93337e05
Gustavo Rizzon <30003088+gustavorizzon@users.noreply.github.com>
2023-08-14 01:25:44 +0000

    feat: adicionando página de `em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <h1>Teste</h1>;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        flex: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        rowGap: "16px",
+      }}
+    >
+      <img
+        src="/img/under_construction.png"
+        style={{ maxWidth: "128px", width: "100%" }}
+      />
+      <h2>em construção...</h2>
+    </div>
+  );
 }
