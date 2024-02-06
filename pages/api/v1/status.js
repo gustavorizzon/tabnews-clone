d1adaa061f469f293b3fd370fa64290d5f3a62b2
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2024-02-05 21:35:56 -0300

    feat: cria seção de database para o endpoint de status

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  return response
-    .status(200)
-    .json({ message: "Alunos do Curso.dev são pessoas acima da média." });
+  const updatedAt = new Date().toISOString();
+  const dbStatusResult = await database.query({
+    text: `
+      SELECT
+        current_setting('server_version')::float as version,
+        current_setting('max_connections')::int AS max_connections,
+        count(*)::int as active_connections
+      FROM pg_stat_activity
+      WHERE datname = $1;
+    `,
+    values: [process.env.POSTGRES_DB],
+  });
+
+  const [dbStatus] = dbStatusResult.rows;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbStatus.version,
+        max_connections: dbStatus.max_connections,
+        active_connections: dbStatus.active_connections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.spec.js
+++ tests/integration/api/v1/status/get.spec.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toEqual(
+    new Date(responseBody.updated_at).toISOString(),
+  );
+
+  const database = responseBody.dependencies.database;
+  expect(database).toBeDefined();
+  expect(database.version).toEqual(expect.any(Number));
+  expect(database.version).toBeGreaterThanOrEqual(16.1);
+  expect(database.max_connections).toEqual(expect.any(Number));
+  expect(database.active_connections).toEqual(1);
 });
