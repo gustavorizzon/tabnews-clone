7638eaa14e4b7eb0748405b15c75293a76b8947b
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2024-04-29 22:54:42 -0300

    fix: corrige bug de conexões abertas

--- pages/api/v1/migrations.js
+++ pages/api/v1/migrations.js
@@ -2,35 +2,36 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
 
+const getDefaultMigrationRunnerConfig = (dbClient, dryRun = true) => ({
+  dbClient,
+  dir: join("infra", "migrations"),
+  direction: "up",
+  dryRun,
+  verbose: true,
+  migrationsTable: "pgmigrations",
+});
+
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationRunnerConfig = {
-    dbClient,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    dryRun: true,
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).end();
+  }
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(
-      defaultMigrationRunnerConfig,
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const liveRun = request.method === "POST";
+    const migrationRunnerConfig = getDefaultMigrationRunnerConfig(
+      dbClient,
+      !liveRun,
     );
+    const migrations = await migrationRunner(migrationRunnerConfig);
+    const statusCode = liveRun && migrations.length > 0 ? 201 : 200;
+    return response.status(statusCode).json(migrations);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await dbClient.end();
-    return response.status(200).json(pendingMigrations);
   }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationRunnerConfig,
-      dryRun: false,
-    });
-    await dbClient.end();
-    return response
-      .status(migratedMigrations.length > 0 ? 201 : 200)
-      .json(migratedMigrations);
-  }
-  await dbClient.end();
-  return response.status(405).end();
 }
