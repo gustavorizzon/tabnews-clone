cff2cbcf15bac8804e482934fab731496b406e76
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2025-05-11 15:50:44 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.spec.js
+++ tests/integration/api/v1/migrations/post.spec.js
@@ -10,33 +10,47 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response.status).toBe(201);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
 
-  const responseBody = await response.json();
+        const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
 
-  for (const migrated of responseBody) {
-    expect(migrated).toEqual({
-      name: expect.any(String),
-      path: expect.any(String),
-      timestamp: expect.any(Number),
+        for (const migrated of responseBody) {
+          expect(migrated).toEqual({
+            name: expect.any(String),
+            path: expect.any(String),
+            timestamp: expect.any(Number),
+          });
+        }
+      });
+
+      it("For the second time", async () => {
+        // Verifica que as migrations não rodaram duas vezes
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const responseBody2 = await response2.json();
+
+        expect(Array.isArray(responseBody2)).toBe(true);
+        expect(responseBody2.length).toBe(0);
+      });
     });
-  }
-
-  // Verifica que as migrations não rodaram duas vezes
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
   });
-  expect(response2.status).toBe(200);
-
-  const responseBody2 = await response2.json();
-
-  expect(Array.isArray(responseBody2)).toBe(true);
-  expect(responseBody2.length).toBe(0);
 });
